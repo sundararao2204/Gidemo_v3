fe5538b0eb1e99249bc8dbe9d8486dde294eeca3
sundararao2204 <sundararao2204@gmail.com>
2025-03-19 08:41:15 +0530

    this is my first commit

---
+++ src/Test1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for the first number
+        System.out.print("Enter the first number: ");
+    	System.out.println("sundararao");
+        double num1 = scanner.nextDouble();
+
+        // Prompt user for the second number
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Perform the addition
+        double sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
